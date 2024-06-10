2dee21ad0876ca8f9dfc0b06dffbf3dd34e59134
xoy <2088437179@qq.com>
2024-06-10 17:58:22 +0800

    添加行星轨迹页面

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def main_handler():
     # 登录密码（用#隔开，例如123456#123456#123456）
     passwd = "xiunan123"
     # 要修改的步数，直接输入想要修改的步数值，留空为随机步数20000至29999之间
-    step = "5988"
+    step = "8203"
 
     user_list = user.split('#')
     passwd_list = passwd.split('#')
